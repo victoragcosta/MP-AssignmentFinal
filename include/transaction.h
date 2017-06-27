066f78c65e8807f25ef0c6d6755434b83a6e9e46
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2017-06-26 21:37:17 -0300

    Implementação das estruturas de dados do módulo de transações.

--- include/transaction.h
+++ include/transaction.h
@@ -12,6 +12,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "avaliacao.h"
 #include "error_level.h"
+#include "product.h"
+
+typedef enum {
+  Friend,
+  FriendOfFriend,
+  Other
+}relationship;
+
+typedef enum {
+  Open,
+  InProgress,
+  Closed,
+  Canceled
+}transactionStatus;
+
+typedef struct userSpecification {
+  relationship proximity;
+  double minimum_rating;
+  double maximum_rating;
+}
+
+typedef struct transaction {
+  unsigned int user1;
+  unsigned int user2;
+  product item;
+  transactionStatus status;
+}
+
+typedef struct transactionList {
+  transaction *items; /**< Items na lista. Vetor de tipo transaction. */
+  int size; /**< Tamanho da lista. Não pode ser negativo. */
+}transactionList;
+
+errorLevel CreateTransaction(unsigned int, unsigned int, product*,
+                             transactionStatus, transaction);
 
 #endif  // TRANSACTION_H_
